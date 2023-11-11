f7026c47d404829b37979d443bc83f25102e3a6b
Hajime <>
2023-11-11 10:25:38 +0800

    fixed bug, refactored js code

--- code.js
+++ code.js
@@ -1,44 +1,64 @@
 //variable define
-//Scene count
-var sceneCount = 1;
 //Score
 var score = 0;
 
 //scene logs
-var scene1log1 = "Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 "
-var scene1log2 = "sample question1 sample question1 sample question1 sample question1 sample question1 sample question1 sample question1";
-var scene1log21 = "sample q1a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ";
-var scene1log22 = "sample q1a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ";
-var scene2log1 = "Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 "
-var scene2log2 = "sample question2 sample question2 sample question2 sample question2 sample question2 sample question2 sample question2";
-var scene2log21 = "sample q2a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ";
-var scene2log22 = "sample q2a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ";
-var scene3log1 = "Sample scene3 Sample scene3 Sample scene3 Sample scene3Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 "
-var scene3log2 = "sample question3 sample question sample question sample question sample question sample question sample question";
-var scene3log21 = "sample q3a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ";
-var scene3log22 = "sample q3a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ";
-var scene4log1 = "Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4Sample scene4 Sample scene4 "
-var scene4log2 = "sample question4 sample question sample question sample question sample question sample question sample question";
-var scene4log21 = "sample q4a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ";
-var scene4log22 = "sample q4a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ";
-var scene5log1 = "Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 "
-var scene5log2 = "sample question5 sample question sample question sample question sample question sample question sample question";
-var scene5log21 = "sample q5a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ";
-var scene5log22 = "sample q5a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ";
-var sceneEnd = "Your day has ended"
 
-var option11 = "Option11"
-var option12 = "Option12"
-var option21 = "Option21"
-var option22 = "Option22"
-var option31 = "Option31"
-var option32 = "Option32"
-var option41 = "Option41"
-var option42 = "Option42"
-var option51 = "Option51"
-var option52 = "Option52"
+var scenes = {
+    scene1: {
+        log1: "Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 Sample scene1 ",
+        log2: "sample question1 sample question sample question sample question sample question sample question sample question",
+        log21: "sample q1a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ",
+        log22: "sample q1a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ",
+        options: {
+            option1: "Option11",
+            option2: "Option12"
+        }
+    },
+    scene2: {
+        log1: "Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 Sample scene2 ",
+        log2: "sample question2 sample question sample question sample question sample question sample question sample question",
+        log21: "sample q2a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ",
+        log22: "sample q2a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ",
+        options: {
+            option1: "Option21",
+            option2: "Option22"
+        }
+    },
+    scene3: {
+        log1: "Sample scene3 Sample scene3 Sample scene3 Sample scene3Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 Sample scene3 ",
+        log2: "sample question3 sample question sample question sample question sample question sample question sample question",
+        log21: "sample q3a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ",
+        log22: "sample q3a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ",
+        options: {
+            option1: "Option31",
+            option2: "Option32"
+        }
+    },
+    scene4: {
+        log1: "Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4 Sample scene4Sample scene4 Sample scene4 ",
+        log2: "sample question4 sample question sample question sample question sample question sample question sample question",
+        log21: "sample q4a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ",
+        log22: "sample q4a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ",
+        options: {
+            option1: "Option41",
+            option2: "Option42"
+        }
+    },
+    scene5: {
+        log1: "Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 Sample scene5 ",
+        log2: "sample question5 sample question sample question sample question sample question sample question sample question",
+        log21: "sample q5a1 sample a1 sample a1 sample a1 sample a1 sample a1 sample a1 ",
+        log22: "sample q5a2 sample a2 sample a2 sample a2 sample a2 sample a2 sample a2 ",
+        options: {
+            option1: "Option51",
+            option2: "Option52"
+        }
+    },
+    sceneEnd: "Your day has ended"
+};
 
-var sceneNumlogNum = scene1log1;
+var currentScene = scenes.scene1.log1;
 
 
 function reset(){
@@ -53,8 +73,8 @@ $(".list-start").click(function(){
     $("#home").hide();
     $("#play").show();
     $("#option").hide();
-    sceneNumlogNum = scene1log1;
-    actLog(sceneNumlogNum);
+    currentScene = scenes.scene1.log1;
+    actLog(currentScene);
 });
 $(".list-howto").click(function(){
     $("#home").hide();
@@ -68,136 +88,110 @@ $(".btn-back").click(function(){
     reset();
 });
 //option select
+
 $(".log").click(function(){
-    if (sceneNumlogNum = scene1log1) {
-        sceneNumlogNum = scene1log2;
-        actLog(sceneNumlogNum);
-        document.getElementById("option1").innerText = option11;
-        document.getElementById("option2").innerText = option12;
-        $("#option").show();
-        console.log(sceneNumlogNum);
-    }else if (sceneNumlogNum = scene1log21) {
-        console.log(sceneNumlogNum);
-        sceneNumlogNum = scene2log1;
-        actLog(sceneNumlogNum);
-    }else if (sceneNumlogNum = scene1log22){
-        console.log(sceneNumlogNum);
-        sceneNumlogNum = scene2log1;
-        actLog(sceneNumlogNum);
-    }else if(sceneNumlogNum = scene2log1) {
-        sceneNumlogNum = scene2log2;
-        actLog(sceneNumlogNum);
-        document.getElementById("option1").innerText = option21;
-        document.getElementById("option2").innerText = option22;
-        $("#option").show();
-    }else if (sceneNumlogNum = scene2log21 || scene2log22) {
-        sceneNumlogNum = scene3log1;
-        actLog(sceneNumlogNum);
-    }else if (sceneNumlogNum == scene3log1) {
-        sceneNumlogNum = scene3log2;
-        actLog(sceneNumlogNum);
-        document.getElementById("option1").innerText = option31;
-        document.getElementById("option2").innerText = option32;
-        $("#option").show();
-    }else if (sceneNumlogNum == scene3log21 || sceneNumlogNum == scene3log22) {
-        sceneNumlogNum = scene4log1;
-        actLog(sceneNumlogNum);
-    }else if (sceneNumlogNum == scene4log1) {
-        sceneNumlogNum = scene4log2;
-        actLog(sceneNumlogNum);
-        document.getElementById("option1").innerText = option41;
-        document.getElementById("option2").innerText = option42;
-        $("#option").show();
-    }else if (sceneNumlogNum == scene4log21 || sceneNumlogNum == scene4log22) {
-        sceneNumlogNum = scene5log1;
-        actLog(sceneNumlogNum);
-    }else if (sceneNumlogNum == scene5log1) {
-        sceneNumlogNum = scene5log2;
-        actLog(sceneNumlogNum);
-        document.getElementById("option1").innerText = option51;
-        document.getElementById("option2").innerText = option52;
-        $("#option").show();
-    }else if (sceneNumlogNum == scene5log21 || sceneNumlogNum == scene5log22) {
-        sceneNumlogNum = sceneEnd;
-        actLog(sceneNumlogNum);
+    if (currentScene === scenes.scene1.log1) {
+        currentScene = scenes.scene1.log2;
+        actLog(currentScene);
+        setOptions(scenes.scene1.options);
+    } else if (currentScene === scenes.scene1.log21 || currentScene === scenes.scene1.log22) {
+        currentScene = scenes.scene2.log1;
+        actLog(currentScene);
+    } else if (currentScene === scenes.scene2.log1) {
+        currentScene = scenes.scene2.log2;
+        actLog(currentScene);
+        setOptions(scenes.scene2.options);
+    } else if (currentScene === scenes.scene2.log21 || currentScene === scenes.scene2.log22) {
+        currentScene = scenes.scene3.log1;
+        actLog(currentScene);
+    } else if (currentScene === scenes.scene3.log1) {
+        currentScene = scenes.scene3.log2;
+        actLog(currentScene);
+        setOptions(scenes.scene3.options);
+    } else if (currentScene === scenes.scene3.log21 || currentScene === scenes.scene3.log22) {
+        currentScene = scenes.scene4.log1;
+        actLog(currentScene);
+    } else if (currentScene === scenes.scene4.log1) {
+        currentScene = scenes.scene4.log2;
+        actLog(currentScene);
+        setOptions(scenes.scene4.options);
+    } else if (currentScene === scenes.scene4.log21 || currentScene === scenes.scene4.log22) {
+        currentScene = scenes.scene5.log1;
+        actLog(currentScene);
+    } else if (currentScene === scenes.scene5.log1) {
+        currentScene = scenes.scene5.log2;
+        actLog(currentScene);
+        setOptions(scenes.scene5.options);
+    } else if (currentScene === scenes.scene5.log21 || currentScene === scenes.scene5.log22) {
+        currentScene = scenes.sceneEnd;
+        actLog(currentScene);
+    } else if (currentScene === scenes.sceneEnd) {
         $("#play").hide();
         $("#result").show();
         document.getElementById("js-score").innerText = score;
     }
 });
 //option show
+function setOptions(options) {
+    document.getElementById("option1").innerText = options.option1;
+    document.getElementById("option2").innerText = options.option2;
+    $("#option").show();
+}
 $(".option1").click(function(){
-    if (sceneNumlogNum = scene1log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene1log21;
-        actLog(sceneNumlogNum);
-        score = score + 50;
-        console.log(score);
-    }else if (sceneNumlogNum = scene2log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene2log21;
-        actLog(sceneNumlogNum);
-        score = score + 20;
-        console.log(score);
-    }else if (sceneNumlogNum = scene3log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene3log21;
-        actLog(sceneNumlogNum);
-        score = score + 20;
-        console.log(score);
-    }else if (sceneNumlogNum = scene4log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene4log21;
-        actLog(sceneNumlogNum);
-        score = score + 50;
-        console.log(score);
-    }else if (sceneNumlogNum = scene5log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene5log21;
-        actLog(sceneNumlogNum);
-        score = score + 20;
-        console.log(score);
+    $("#option").hide();
+    // 各オプションごとの処理を追加
+    if (currentScene === scenes.scene1.log2) {
+        currentScene = scenes.scene1.log21;
+        actLog(currentScene);
+        score += 50;
+    } else if (currentScene === scenes.scene2.log2) {
+        currentScene = scenes.scene2.log21;
+        actLog(currentScene);
+        score += 20;
+    } else if (currentScene === scenes.scene3.log2) {
+        currentScene = scenes.scene3.log21;
+        actLog(currentScene);
+        score += 20;
+    } else if (currentScene === scenes.scene4.log2) {
+        currentScene = scenes.scene4.log21;
+        actLog(currentScene);
+        score += 50;
+    } else if (currentScene === scenes.scene5.log2) {
+        currentScene = scenes.scene5.log21;
+        actLog(currentScene);
+        score += 20;
     }
+    console.log(score);
 });
-
 $(".option2").click(function(){
-    if (sceneNumlogNum = scene1log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene1log22;
-        console.log(sceneNumlogNum)
-        actLog(sceneNumlogNum);
-        score = score + 20;
-        console.log(score);
-    }else if (sceneNumlogNum = scene2log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene2log22;
-        actLog(sceneNumlogNum);
-        score = score + 50;
-        console.log(score);
-    }else if (sceneNumlogNum = scene3log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene3log22;
-        actLog(sceneNumlogNum);
-        score = score + 50;
-        console.log(score);
-    }else if (sceneNumlogNum = scene4log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene4log22;
-        actLog(sceneNumlogNum);
-        score = score + 20;
-        console.log(score);
-    }else if (sceneNumlogNum = scene5log2) {
-        $("#option").hide();
-        sceneNumlogNum = scene5log22;
-        actLog(sceneNumlogNum);
-        score = score + 50;
-        console.log(score);
+    $("#option").hide();
+    // 各オプションごとの処理を追加
+    if (currentScene === scenes.scene1.log2) {
+        currentScene = scenes.scene1.log22;
+        actLog(currentScene);
+        score += 20;
+    } else if (currentScene === scenes.scene2.log2) {
+        currentScene = scenes.scene2.log22;
+        actLog(currentScene);
+        score += 50;
+    } else if (currentScene === scenes.scene3.log2) {
+        currentScene = scenes.scene3.log22;
+        actLog(currentScene);
+        score += 50;
+    } else if (currentScene === scenes.scene4.log2) {
+        currentScene = scenes.scene4.log22;
+        actLog(currentScene);
+        score += 20;
+    } else if (currentScene === scenes.scene5.log2) {
+        currentScene = scenes.scene5.log22;
+        actLog(currentScene);
+        score += 50;
     }
+    console.log(score);
 });
-
 //log animation
 
-function actLog(sceneNumlogNum){
+function actLog(currentScene){
     document.getElementById("log").innerText = "";
     const log = (param) => {
 
@@ -212,7 +206,7 @@ function actLog(sceneNumlogNum){
     };
     log({
         el: "#log", //要素
-        string: sceneNumlogNum, //文字列
+        string: currentScene, //文字列
         speed: 50 //速度
     });    
 }
